2ca3d6ad14d0eba788eedd42af076deed9374a34
Matt Welke <mattwelke@gmail.com>
2021-12-19 21:56:41 -0500

    Update App.java

--- src/main/java/com/mattwelke/App.java
+++ src/main/java/com/mattwelke/App.java
@@ -7,20 +7,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class App {
 
     public static void main(String[] args) {
-        Instant start = Instant.now();
-
-        AtomicInteger num = new AtomicInteger();
-
-        for (int i = 0; i < 100000; i++) {
-            Runnable r = num::getAndIncrement;
-            Thread.ofVirtual().start(() -> System.out.println("Hello world from a virtual thread."));
-        }
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Time: " + Duration.between(Instant.now(), start));
+        Thread.ofVirtual().start(() -> System.out.println("Hello world from a virtual thread."));
     }
 }
